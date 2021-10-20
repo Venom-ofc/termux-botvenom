2d3466be84ca5f04027a3fa293fa474ca7458e45
Venom-ofc <venommods9@gmail. com>
2021-10-20 14:08:36 -0400

    Fist commit

---
+++ src/help.js
@@ -0,0 +1,136 @@
+const help = (prefix) => {
+	return `╭─《→ 👋Olá bemvindo ao menu\n*│💎Venombot-ofc💎*
+ *└────────\n	
+ *╭─《→ *⚠️Créditos⚠* 》
+ 
+➪⚠ _https://chat.whatsapp.com/LTyM3PdbIw3CyLmy0wF9EQ_
+
+➪⚠ _https://venom-apis.herokuapp.com 
+
+➪⚠ _https://github.com/Venom-ofc
+
+➪⚠ ️ _https://youtube.com/channel/UCOoc5DOT_M6foZa1jSOI6JQ_
+
+➪⚠  _https://wa.me/+37122291651_
+
+*╭─《*⚠Base By Venom Mods⚠*
+*│*
+*╭─《> Comandos de Figurinhas <》*
+*│*
+*│*➪🇧🇷→comando : *${prefix}sticker* ou *${prefix}figu*
+*│*➪✎desc : converter imagem / gif / vídeo em figurinha
+*│*➪✎como usar : responder imagem / gif / vídeo, ou enviar imagem / gif / vídeo com legenda ${prefix}figu
+*│*
+*│*➪🇧🇷→comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+*│*➪✎desc : converter imagem em figurinha removendo o fundo
+*│*➪✎como usar : marque imagens, ou envie imagens com a legenda ${prefix}sticker nobg
+*│*
+*│*➪🇧🇷→comando : *${prefix}toimg*
+*│*➪✎desc : converte  a figurinhas  em foto
+*│*➪✎como usar : marque a figurinha
+*│*
+*│*➪🇧🇷→comando : *${prefix}tsticker* or *${prefix}tstiker*
+*│*➪✎descrição : converte  texto em figurinha
+*│*➪✎como usar : *${prefix}tsticker text in here*
+*│*
+*└──────────────────*	
+
+*╭─《> *Mais alguns comandos* <》
+*│*
+*│*➪🇧🇷→comando : *${prefix}tp*
+*│*➪✎descrição : Criador de texto / logotipo do textpro.me
+*│*➪✎como usar : *${prefix}tp [sim] [texto]*\exemplo : *${prefix}tp 1 Seu texto*
+*│*➪⚠️aviso : *${prefix}tp list* ( para mostrar todo os tema )
+*│*
+*│*➪🇧🇷→comando : *${prefix}ep*
+*│*➪✎descrição : Texto / Criador de logotipos de ephoto360.com
+*│*➪✎como usar : *${prefix}ep [no] [text]*\nexample : *${prefix}ep 1 Seu texto*
+*│*➪⚠️aviso : *${prefix}ep list* ( para mostrar todo os tema )
+*│*
+*│*➪🇧🇷→comando : *${prefix}tahta*
+*│*➪✎descrição : Tesouro do trono.....
+*│*➪✎como usar : *${prefix}tahta [text]*\nExemplo : *${prefix}tahta Seu texto*
+*│*
+*│*➪🇧🇷→comando : *${prefix}gtts*
+*│*➪✎descrição : converte  texto em/audio
+*│*➪✎como usar : *${prefix}gtts [cc] [text]*\nExemplo : *${prefix}gtts pt Fala bor*
+*│*
+*│*➪🇧🇷→comando : *${prefix}url2img*
+*│*➪✎descrição : tirar capturas de tela da web
+*│*➪✎como usar : *${prefix}url2img [tipe] [url]*
+*│*
+*│*➪🇧🇷→comando : *${prefix}simi*
+*│*➪✎descrição : sua mensagem será respondida por simi
+*│*➪✎como usar : *${prefix}simi sua mensagem*
+*│*
+*│*➪🇧🇷→comando : *${prefix}ocr*
+*│*➪✎descrição : pegue o texto de uma foto
+*│*➪✎como usar : responder imagem ou enviar imagem com legenda ${prefix}ocr
+*│*
+*│*➪🇧🇷→comando : *${prefix}wait*
+*│*➪✎descrição : pesquisar anime com imagem [Nome / apelido]
+*│*➪✎como usar : responder imagem ou enviar imagem com legenda ${prefix}wait
+*│*
+*│*➪🇧🇷→comando : *${prefix}setprefix*
+*│*➪✎descrição : mudar prefixo do bot
+*│*➪✎como usar : *${prefix}setprefix [texto|prefixo]*\nExemplo  : *${prefix}setprefix #*
+*│*
+*│*➪⚠️aviso : Este comando só pode ser usado pelo proprietário do bot
+*│*
+*└──────────────────*	
+
+*╭─《> *Comandos de grupo* <》
+*│*
+*│*➪🇧🇷→comando : *${prefix}add*
+*│*➪✎descrição : Adicionar membros no grupo
+*│*➪✎como usar : *${prefix}add 559791xxxxx*
+*│*
+*│*➪⚠️aviso : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin !
+*│*
+*│*➪🇧🇷→comando : *${prefix}kick*
+*│*➪✎descrição : Remover Membros do grupo
+*│*➪✎como usar : *${prefix}kick @tag do membro*
+*│*
+*│*➪⚠️aviso : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+*│*
+*│*➪🇧🇷→comando : *${prefix}promote*
+*│*➪✎descrição : tornar o membro do grupo como administrador do grupo
+*│*➪✎como usar : *${prefix}promote @tag do membro*
+*│*
+*│*➪⚠️aviso : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+*│*
+*│*➪🇧🇷→comando : *${prefix}demote*
+*│*➪✎descrição : tornar o administrador do grupo como membro do grupo comum
+*│*➪✎como usar : *${prefix}demote @tag do membro*
+*│*
+*│*➪⚠️aviso : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+*│*
+*│*➪🇧🇷→comando : *${prefix}linkgroup*
+*│*➪✎descrição : o bot Envia o link do grupo
+*│*➪✎como usar : mande apenas o comando 
+*│*➪⚠️aviso : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+*│*
+*│*➪🇧🇷→comando : *${prefix}leave*
+*│*➪✎descrição : Use para o bot saur do grupo
+*│*➪✎como usar : mande apenas o comando 
+*│*➪⚠️aviso : Só pode ser usado por administradores de grupo e proprietário do bot
+*│*➪🇧🇷→comando : *${prefix}tagall*
+*│*➪✎descrição : marca todos os membros do grupo, incluindo administradores também
+*│*➪✎como usar : mande apenas o comando
+*│*➪⚠️aviso : Este comando pode ser usado se você for um administrador de grupo
+*│*➪🇧🇷→comando : *${prefix}simih*
+*│*➪✎descrição : Ativar modo simih no grupo
+*│*➪✎como usar : *${prefix}simih 1* para ativar o modo simi  *${prefix}simih 0* para Desativar o modo simih
+*│*➪⚠️aviso : Este comando pode ser usado se você for um administrador de grupo
+*└──────────────────*\n	
+
+*╭─《*⚠Base By Venom Mods⚠*
+*│*
+*│*➪→ *💎Venombot-ofc💎*
+*│*
+*└──────────────────*
+️`
+}
+
+exports.help = help
+
